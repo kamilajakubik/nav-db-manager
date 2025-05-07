734711c62a433020132ee111678c04403af9ba36
kamilajakubik <kamilajakubik1@gmail.com>
2025-05-07 20:36:22 +0200

    correct ArincFile model

--- data_processor/models.py
+++ data_processor/models.py
@@ -8,10 +8,12 @@ class ArincFile(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20, default="PENDING")
     processing_errors = models.TextField(blank=True, null=True)
-    cycle = models.ForeignKey(DataCycle, on_delete=models.SET_NULL, null=True, blank=True)
+    cycle = models.ForeignKey(
+        DataCycle, on_delete=models.SET_NULL,  null=True, blank=True, related_name='files'
+    )
 
     class Meta:
-        ordering = ['uploaded_at']
+        ordering = ['-uploaded_at']
 
     def __str__(self):
-        return f"{self.file.name} - {self.status}"
+        return f"{self.file.name} - {self.status} - cycle {self.cycle}"
