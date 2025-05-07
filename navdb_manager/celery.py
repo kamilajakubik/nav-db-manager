d3fcbfbc3d26d6fef0ffc7bce0a95ed7596c13ca
kamilajakubik <kamilajakubik1@gmail.com>
2025-05-07 21:11:49 +0200

    add celery config

---
+++ navdb_manager/celery.py
@@ -0,0 +1,8 @@
+import os
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "navdb_maager.settings")
+
+celery = Celery("navdb_manager")
+celery.config_from_object("django.conf::settings", namespace="CELERY")
+celery.autodiscover_tasks()
