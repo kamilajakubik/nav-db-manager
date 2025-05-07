f2bacaabd5f06475df75b42f9448f8be02df908c
kamilajakubik <kamilajakubik1@gmail.com>
2025-05-07 21:12:57 +0200

    correct celery settings

--- navdb_manager/celery.py
+++ navdb_manager/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "navdb_maager.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "navdb_manager.settings")
 
 celery = Celery("navdb_manager")
 celery.config_from_object("django.conf::settings", namespace="CELERY")
